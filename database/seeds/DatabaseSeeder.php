764f89cb9f90204c636ba2e3950cbfde41c0cec5
Barahona <adonaybarahona97@gmail.com>
2018-10-05 14:49:14 -0600

    DeptSeed y MunSeed completos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(DepartamentosTableSeeder::class);
         $this->call(MunicipiosTableSeeder::class);
         
     }
--- database/seeds/DepartamentosTableSeeder.php
+++ database/seeds/DepartamentosTableSeeder.php
@@ -13,5 +13,62 @@ class DepartamentosTableSeeder extends Seeder
      */
     public function run()
     {
+        Departamento::create([
+            'id' => 'SV-AH',
+            'nDepartamento' => 'Ahuachapán'
+            ]);
+        Departamento::create([
+            'id' => 'SV-CA',
+            'nDepartamento' => 'Cabañas'
+            ]);
+        Departamento::create([
+            'id' => 'SV-CH',
+            'nDepartamento' => 'Chalatenango'
+            ]);
+        Departamento::create([
+            'id' => 'SV-CU',
+            'nDepartamento' => 'Cuscatlán'
+            ]);
+        Departamento::create([
+            'id' => 'SV-LI',
+            'nDepartamento' => 'La Libertad'
+            ]);
+        Departamento::create([
+            'id' => 'SV-PA',
+            'nDepartamento' => 'La Paz'
+            ]);
+        Departamento::create([
+            'id' => 'SV-UN',
+            'nDepartamento' => 'La Unión'
+            ]);
+        Departamento::create([
+            'id' => 'SV-MO',
+            'nDepartamento' => 'Morazán'
+            ]);
+        Departamento::create([
+            'id' => 'SV-SM',
+            'nDepartamento' => 'San Miguel'
+            ]);
+        Departamento::create([
+            'id' => 'SV-SS',
+            'nDepartamento' => 'San Salvador'
+            ]);
+        Departamento::create([
+            'id' => 'SV-SV',
+            'nDepartamento' => 'San Vicente'
+            ]);
+        Departamento::create([
+            'id' => 'SV-SA',
+            'nDepartamento' => 'Santa Ana'
+            ]);
+        Departamento::create([
+            'id' => 'SV-SO',
+            'nDepartamento' => 'Sonsonate'
+            ]);
+        Departamento::create([
+            'id' => 'SV-US',
+            'nDepartamento' => 'Usulután'
+            ]);
+        
     }
 }
