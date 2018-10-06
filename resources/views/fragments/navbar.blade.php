e834a57345345b01877592d73eeece8648afc204
Barahona <adonaybarahona97@gmail.com>
2018-10-05 20:24:23 -0600

    Bs 4 home - login fix

--- resources/views/fragments/navbar.blade.php
+++ resources/views/fragments/navbar.blade.php
@@ -21,14 +21,18 @@
                 <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
                 <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
             @else
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a href="{{ route('logout') }}" class="dropdown-item"
-                           onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
+            <li class="dropdown nav-link">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                    {{ Auth::user()->nombres }} <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();" class="dropdown-item">Cerrar Sesion</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                    </li>
+                </ul>
+            </li>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
                               style="display: none;">
@@ -41,3 +45,4 @@
     </div>
 
 </nav>
+
