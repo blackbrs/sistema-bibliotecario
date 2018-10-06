791c28f0da3b23daae1b55c2887d92a2b2a09c67
Barahona <adonaybarahona97@gmail.com>
2018-10-05 21:44:49 -0600

    Fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,94 +2,94 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Registro de usuario</div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="panel-body">
+                <div class="card-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('nombres') ? ' has-error' : '' }}">
-                            <label for="nombres" class="col-md-4 control-label">Nombres</label>
+                        <div class="form-group row">
+                            <label for="nombres" class="col-md-4 col-form-label text-md-right">Nombres</label>
 
                             <div class="col-md-6">
 
-                                <input id="nombres" type="text" class="form-control" name="nombres" value="{{ old('nombres') }}" required autofocus>
+                                <input id="nombres" type="text" class="form-control{{ $errors->has('nombre') ? ' has-error' : '' }}" name="nombres" value="{{ old('nombres') }}" required autofocus>
 
                                 @if ($errors->has('nombres'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('nombres') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
                         
-                        <div class="form-group{{ $errors->has('apellidos') ? ' has-error' : '' }}">
-                            <label for="apellidos" class="col-md-4 control-label">Apellidos</label>
+                        <div class="form-group row">
+                            <label for="apellidos" class="col-md-4 col-form-label text-md-right">Apellidos</label>
 
                             <div class="col-md-6">
-                                <input id="apellidos" type="text" class="form-control" name="apellidos" value="{{ old('apellidos') }}" required autofocus>
+                                <input id="apellidos" type="text" class="form-control{{ $errors->has('apellidos') ? ' has-error' : '' }}" name="apellidos" value="{{ old('apellidos') }}" required autofocus>
 
-                                @if ($errors->has('apellidos'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('apellidos') }}</strong>
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
                         
-                        <div class="form-group{{ $errors->has('nacimiento') ? ' has-error' : '' }}">
-                            <label for="nacimiento" class="col-md-4 control-label">Fecha de nacimiento</label>
+                        <div class="form-group row">
+                            <label for="nacimiento" class="col-md-4 col-form-label text-md-right">Fecha de nacimiento</label>
 
                             <div class="col-md-6">
-                            <input id="nacimiento" type="text" class="form-control" name="nacimiento" value="{{ old('nacimiento') }}"  placeholder="YYYY-MM-DD" required autofocus>
+                            <input id="nacimiento" type="text" class="form-control{{ $errors->has('nacimiento') ? ' has-error' : '' }}" name="nacimiento" value="{{ old('nacimiento') }}"  placeholder="YYYY-MM-DD" required autofocus>
 
                                 @if ($errors->has('nacimiento'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('nacimiento') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('sexo') ? ' has-error' : '' }}">
-                            <label for="sexo" class="col-md-4 control-label">Sexo</label>
+                        <div class="form-group row">
+                            <label for="sexo" class="col-md-4 col-form-label text-md-right">Sexo</label>
 
                             <div class="col-md-6">
-                            <input id="sexo" type="text" class="form-control" name="sexo" value="{{ old('sexo') }}"   required autofocus>
+                            <input id="sexo" type="text" class="form-control{{ $errors->has('sexo') ? ' has-error' : '' }}" name="sexo" value="{{ old('sexo') }}"   required autofocus>
 
                                 @if ($errors->has('sexo'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('sexo') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('Npadres') ? ' has-error' : '' }}">
-                            <label for="Npadres" class="col-md-4 control-label">Nombre del papa o mama</label>
+                        <div class="form-group row">
+                            <label for="Npadres" class="col-md-4 col-form-label text-md-right">Nombre del papa o mama</label>
 
                             <div class="col-md-6">
-                            <input id="Npadres" type="text" class="form-control" name="Npadres" value="{{ old('Npadres') }}"   required autofocus>
+                            <input id="Npadres" type="text" class="form-control{{ $errors->has('Npadres') ? ' has-error' : '' }}" name="Npadres" value="{{ old('Npadres') }}"   required autofocus>
 
                                 @if ($errors->has('Npadres'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('Npadres') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('telefono') ? ' has-error' : '' }}">
-                            <label for="telefono" class="col-md-4 control-label">Numero de telefono</label>
+                        <div class="form-group row">
+                            <label for="telefono" class="col-md-4 col-form-label text-md-right">Numero de telefono</label>
 
                             <div class="col-md-6">
-                            <input id="telefono" type="numeric" class="form-control" name="telefono" value="{{ old('telefono') }}"   required number>
+                            <input id="telefono" type="numeric" class="form-control{{ $errors->has('telefono') ? ' has-error' : '' }}" name="telefono" value="{{ old('telefono') }}"   required number>
 
                                 @if ($errors->has('telefono'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('telefono') }}</strong>
                                     </span>
                                 @endif
@@ -97,44 +97,44 @@
                         </div>
                         
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo Electronico</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Correo Electronico</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' has-error' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirme su contraseña</label>
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirme su contraseña</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Registrarse
                                 </button>
