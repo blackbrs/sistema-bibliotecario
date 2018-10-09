ed6c0fd7b1c9fac8f17b09f6ee0b8e34a9d56f48
Barahona <adonaybarahona97@gmail.com>
2018-10-08 18:50:42 -0600

    hot fix 4

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -101,7 +101,7 @@
                         <div class="form-group row">
                             <label for="dept" class="col-md-4 col-form-label text-md-right">Departamento</label>
                             <div class="col-md-6">
-                                <select name="dept" id= "dep_id" class="form-control dynamic" data-dependent="municipo_id" required>
+                                <select name="dept" id= "dep_id" class="form-control dynamic" data-dependent="nMunicipio" required>
                                     <option value="">--Departamento--</option>
                                     @foreach ($lista_dep as $dep)
                                     <option value="{{ $dep->id }}"> {{ $dep->nDepartamento }}</option>  
@@ -110,27 +110,13 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="municipio_id" class="col-md-4 col-form-label text-md-right">Municipio</label>
+                            <label for="nMunicipio" class="col-md-4 col-form-label text-md-right">Municipio</label>
                             <div class="col-md-6">
-                                <select name="municipio_id" id="municipo_id" class="form-control" required>
+                                <select name="nMunicipio" id="nMunicipio" class="form-control">
                                  <option>--Municipio--</option>
                              </select>
                          </div>
                         </div>
-                        <div class="form-group row">
-                         <label for="biblioteca_id" class="col-md-4 col-form-label text-md-right">Biblioteca a inscribirse</label>
-                            <div class="col-md-6">
-                            <select name="biblioteca_id" id= "biblioteca_id" class="form-control" required>
-                
-                                    <option value="">--Biblioteca--</option>
-                                    @foreach ($biblioteca as $bib)
-                                    <option value="{{ $bib->id }}"> {{ $bib->nombreBiblioteca}} </option>  
-                                    @endforeach
-                                </select>  
-                            </div>
-                        </div>
-
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">Correo Electronico</label>
 
