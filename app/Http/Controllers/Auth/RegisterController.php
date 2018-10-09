9eb12eeb2249db9de831a28140d335f256e7d9a1
Barahona <adonaybarahona97@gmail.com>
2018-10-08 18:38:01 -0600

    fix municipio 2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'email.required'        =>'El campo de correo electronico es obligatorio',
             'password.required'     =>'El campo de contraseña es obligatorio',
             'password.min'          =>'La contraseña debe poseer almenos 6 caracteres',
-            'nMunicipio.required'   =>'El campo municipio es obligatorio',
+            'municipio_id.required'   =>'El campo municipio es obligatorio',
             'biblioteca_id.required'    =>'La biblioteca es requerida'
         ]);
     }
@@ -91,7 +91,7 @@ class RegisterController extends Controller
             'telefono'      => $data['telefono'],
             'email'         => $data['email'],
             'password'      => bcrypt($data['password']),
-            'municipio_id'     => $data['nMunicipio'],
+            'municipio_id'     => $data['municipio_id'],
             'biblioteca_id'    =>$data['biblioteca_id']
         ]);
     }
