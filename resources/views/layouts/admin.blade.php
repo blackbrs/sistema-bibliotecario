6fd35b66069ad534501aa75e81c20c9c8e84d75c
Guillermo <guillermobrs@gmail.com>
2018-12-01 22:03:33 -0600

    cambios  minimos

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,7 +16,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'SBM - ADMIN') }}</title>
 
 </head>
 <body class="hold-transition sidebar-mini">
