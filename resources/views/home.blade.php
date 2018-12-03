1abd34ee9ac1519af6c52467737eba26f98b4f6e
Guillermo <guillermobrs@gmail.com>
2018-12-03 04:08:35 -0600

    MEJOR INICIO

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
 <div class="container">
-    
-    
+        <div class="row justify-content-center">
+        <div class="card">
+                <div class="card-header">Bienvenido Al sistema Bibliotecario</div>
+                <div class="card-body">
+                        <img src="img/ues.jpg" class="img-fluid" alt="Responsive image">
+                    </div> 
+                
+              </div>
+            </div>
 </div>
 @endsection
